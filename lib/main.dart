0401c782bc12810c5c2e36ed1fd2750deeb567d6
Samuel <yusheng593@gmail.com>
2024-09-13 22:14:36 +0800

    ThemeData

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,39 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+// rgb(41, 95, 152)
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 41, 95, 152),
+);
+
 void main() {
-  runApp(const MaterialApp(
-    home: Expenses(),
+  runApp(MaterialApp(
+    theme: ThemeData().copyWith(
+      colorScheme: kColorScheme,
+      appBarTheme: const AppBarTheme().copyWith(
+        backgroundColor: kColorScheme.onPrimaryContainer,
+        foregroundColor: kColorScheme.primaryContainer,
+      ),
+      cardTheme: const CardTheme().copyWith(
+        color: kColorScheme.secondaryContainer,
+        margin: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 8,
+        ),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: kColorScheme.primaryContainer,
+        ),
+      ),
+      textTheme: ThemeData().textTheme.copyWith(
+            titleLarge: TextStyle(
+              fontWeight: FontWeight.normal,
+              color: kColorScheme.onSecondaryContainer,
+              fontSize: 14,
+            ),
+          ),
+    ),
+    home: const Expenses(),
   ));
 }
